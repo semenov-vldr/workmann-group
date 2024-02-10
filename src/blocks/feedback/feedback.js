d4e9bd922ea6d6bae17b06e5a6c016177ca0aff6
Vladimir <vova2066@yandex.ru>
2024-02-11 02:22:13 +0300

    add arrow dropdown

--- src/blocks/feedback/feedback.js
+++ src/blocks/feedback/feedback.js
@@ -2,16 +2,20 @@ const feedback = document.querySelector(".feedback");
 
 if (feedback) {
 
+  const formGroupDropdown = document.querySelector(".form__group--dropdown");
+
   const invTypeInput = document.querySelector("#inv-type");
   const dropdown = document.querySelector(".form__dropdown");
 
   invTypeInput.addEventListener("click", () => {
     dropdown.classList.add('visible');
+    formGroupDropdown.classList.add('selected');
   })
 
   document.addEventListener("click", (evt) => {
     if (evt.target !== invTypeInput) {
       dropdown.classList.remove('visible');
+      formGroupDropdown.classList.remove('selected');
     }
 
     const selectedType = document.querySelector("input[type='radio']:checked");
