326b9206e90c3f61c80fc01b14a68154f82d4777
Vladimir <vova2066@yandex.ru>
2024-02-19 23:30:44 +0300

    add hidden bottom

--- src/blocks/popup-type/popup-type.js
+++ src/blocks/popup-type/popup-type.js
@@ -2,11 +2,19 @@ const popupTypeBtn = document.querySelector(".popup-type-btn");
 
 if (popupTypeBtn) {
 
-  //window.localStorage.clear();
-
   const popupTypeBtnValue = popupTypeBtn.querySelector(".popup-type-btn-value");
   const popupType = document.querySelector(".popup-type");
   const popupTypeList = popupType.querySelectorAll(".popup-type-item");
+
+  const popupTypeBottom = popupType.querySelectorAll(".popup-type__confirm, .popup-type__actions");
+
+  const handlePopupTypeBottom = (isVisible) => {
+    popupTypeBottom.forEach(bottomItem => {
+      bottomItem.style.display = `${isVisible? "" : "none"}`;
+    });
+  };
+
+  handlePopupTypeBottom (false);
 
   const startedType = window.localStorage.getItem('investorType');
 
@@ -23,6 +31,7 @@ if (popupTypeBtn) {
   if (startedType) {
     popupTypeBtnValue.textContent = window.localStorage.getItem('investorType');
     setTypeValue (startedType);
+    popupTypeBottom.forEach(bottomItem => bottomItem.style.display = "");
   }
 
   popupTypeBtn.addEventListener("click", () => {
@@ -39,9 +48,16 @@ if (popupTypeBtn) {
       window.localStorage.setItem("investorType", dataTypeItem);
       selectedType = window.localStorage.getItem('investorType');
       popupTypeBtnValue.textContent = selectedType;
-      //this.classList.add('js-type-active');
-
       setTypeValue (selectedType);
+      handlePopupTypeBottom (true);
     });
   });
+
+  const popupTypeBtns = popupType.querySelectorAll(".popup-type__actions button");
+  popupTypeBtns.forEach(popupBtn => {
+    popupBtn.addEventListener("click", () => {
+      popupType.classList.remove("js-active");
+    });
+  });
+
 }
